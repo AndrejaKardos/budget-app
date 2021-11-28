fabde1a780547df4e3d1e3670f22a3ec691eeb0c
AndrejaKardos <andreja.kardos@gmail.com>
2021-11-28 16:29:51 +1100

    Created Calculator component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app__container">
-      <div className="app__container--header">
-        <h2 className="">Budget App</h2>
-      </div>
-      <div className="app__container--body">
-        Calculations here
-      </div>
-      <div className="app__container--footer">
-        Footer here
-      </div>
+    <div className="app">
     </div>
   );
 }
