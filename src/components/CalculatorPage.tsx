52c79944a17f8e37e6246d9af281f4b887cb151c
AndrejaKardos <andreja.kardos@gmail.com>
2021-12-03 17:47:08 +1100

    Added payment frequency select

--- src/components/CalculatorPage.tsx
+++ src/components/CalculatorPage.tsx
@@ -9,6 +9,7 @@ interface Props {
 const CalculatorPage: React.FC<Props> = (props) => {
     const [income, setIncome] = useState<number>(0);
     const [incomeTax, setIncomeTax] = useState<number>(0);
+    const [paymentFrequency, setPaymentFrequency] = useState<number>(1);
 
     const { send: sendIncomeTaxCalcRequest, ...incomeTaxCalcState } = useApiRequest<number, number>(incomeTaxApi.calculateIncomeTax)
 
@@ -30,10 +31,22 @@ const CalculatorPage: React.FC<Props> = (props) => {
             <div>Please enter your annual income ($):</div>
             <input value={income} onChange={(e) => onIncomeChange(e.currentTarget.value)} />
 
+            <select value={paymentFrequency} onChange={(e) => setPaymentFrequency(parseInt(e.currentTarget.value))}>
+              <option value={1}>annually</option>
+              <option value={12}>monthly</option>
+              <option value={52}>weekly</option>
+            </select>
+
             <div>Your income is: {formatMoney(income)}</div>
 
             { incomeTaxCalcState.isLoading && <div>We are calculating your income tax...</div>}
-            { incomeTaxCalcState.isOk && <div>You are paying {formatMoney(incomeTax)} in income tax</div>}
+            { incomeTaxCalcState.isOk && 
+              <>
+                <div>You are paying {formatMoney(incomeTax)} in income tax</div>
+                <div>You are receiving {formatMoney(income - incomeTax)} income after tax</div>
+                <div>You are receiving {formatMoney((income - incomeTax)/paymentFrequency)} income per payment period</div>
+              </>
+            }
         </div>
     );
 }
